0861705b7f33f4407f6dae3376bcaef2f4b3c3c1
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-05-28 10:07:03 +0500

    add calendar

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,41 @@
+'use client'
+import Scheduler from "./Scheduler";
+import Link from "next/link";
+
 export default function Room({ room, studio }) {
-	return (
+    const storagePath = `${studio.id}/${room.id}/books`
+    const tryGetLocalStorageBooks = () => {
+        return typeof window !== 'undefined'
+            ? localStorage.getItem(storagePath)
+                ? JSON.parse(localStorage.getItem(storagePath))
+                : []
+            : []
+    }
+
+    const data = [
+        ...tryGetLocalStorageBooks()
+    ]
+
+    const startTime = +studio.openTime.split(':')[0]
+    const closeTime = +studio.closeTime.split(':')[0] ? +studio.closeTime.split(':')[0] : 24
+
+    return (
         <div className="container">
-            <div className="flex justify-between">
-                <h1 className="text-2xl font-semibold mb-4">{room.title}</h1>
+            <div className="flex justify-between mb-4">
+                <div className="flex items-center gap-10">
+                    <h1 className="text-2xl font-semibold">{room.title}</h1>
+                    <Link href="/" className="text-text/70 block">{studio.title}</Link>
+                </div>
                 <span className="text-lg">Цена: от {room.price} ₽</span>
             </div>
+
             <p>{room.desc}</p>
+
+            <Scheduler
+                from={startTime}
+                to={closeTime}
+                data={data}
+                storagePath={storagePath} />
         </div>
     );
 }
